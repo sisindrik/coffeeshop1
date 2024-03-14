d8207d50fa7dfd9365acbce56a5230c13481b154
sisindrik <sisindrik@lyrostech.com>
2024-03-14 22:28:59 +0530

    first commit

---
+++ src/components/user/Login.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { useContext } from "react";
+import { AppContext } from "../../context/appContext";
+import Register from "./Register";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import "./Login.css";
+import { useState } from "react";
+export default function Login() {
+  
+  const { user, setUser, users, setUsers,flag,setFlag } = useContext(AppContext);
+  const [visible, setvisible] = useState(false);
+  const [msg, setMsg] = useState();
+  const validateUser = () => {
+    const found = users.find(
+      (elem) => elem.email === user.email && elem.pass === user.pass
+    );
+    if (found) {
+      setUser((prev) => ({ ...prev, name: found.name }));
+      setFlag(() => 2);
+    } else setMsg(() => "Invalid email or password");
+  };
+  return (
+    <>
+      {flag === 1 && <Register />}
+      <div className="container">
+        <div className="leftBox">
+          <div className="title">Irish Cafe</div>
+          <br></br>{" "}
+          <div className="tagLine">
+            Brewed with elegance, served with grace.
+          </div>
+        </div>
+        <div className="rightBox">
+          <div className="Login-msg">{msg}</div>
+          <div className="txtBox">
+            <input
+              onChange={(e) =>
+                setUser((prev) => ({ ...prev, email: e.target.value }))
+              }
+              placeholder="Enter Email"
+              autoFocus
+            ></input>
+          </div>
+          <div className="pwdEye">
+            <input
+              onChange={(e) =>
+                setUser((prev) => ({ ...prev, pass: e.target.value }))
+              }
+              type={visible ? "text" : "password"}
+              placeholder="Enter Passcode"
+            ></input>
+            <span onClick={() => setvisible(() => !visible)}>
+              {visible ? <FaEye size={20} /> : <FaEyeSlash size={20} />}
+            </span>
+          </div>
+          <div>
+            <button onClick={validateUser}>Log in</button>
+          </div>
+          <br></br>
+          <div>Forgot Password?</div>
+          <br></br>
+          <div>
+            <button onClick={() => setFlag(() => 1)} className="newAccBtn">
+              Create new account
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
